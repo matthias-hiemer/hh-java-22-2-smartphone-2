88d59f1485e6a5edcd31c4195526cc848cc18cd6
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-09-07 09:51:49 +0200

    Add abstract Contact class

---
+++ src/main/java/model/Contact.java
@@ -0,0 +1,23 @@
+package model;
+
+public abstract class Contact {
+
+    protected String name;
+    protected String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
